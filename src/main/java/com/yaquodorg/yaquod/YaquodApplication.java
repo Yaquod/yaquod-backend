1f94f89c24d8fbc9e1eff088917f2d57c3caaa0d
zkrallah <muhammad.heshamyt@gmail.com>
2025-10-07 16:46:09 +0300

    chore: Initial setup and dependencies of the project.

---
+++ src/main/java/com/yaquodorg/yaquod/YaquodApplication.java
@@ -0,0 +1,13 @@
+package com.yaquodorg.yaquod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaquodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaquodApplication.class, args);
+	}
+
+}
