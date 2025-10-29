b75b7483c45f67f4092eceb73fa15ce66f65b738
zkrallah <muhammad.heshamyt@gmail.com>
2025-10-29 14:26:36 +0300

    test: Generated unit tests for the AuthenticationService logic.

--- src/main/java/com/yaquodorg/yaquod/dtos/GoogleLoginDto.java
+++ src/main/java/com/yaquodorg/yaquod/dtos/GoogleLoginDto.java
@@ -2,9 +2,11 @@ package com.yaquodorg.yaquod.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GoogleLoginDto {
     private String email;
     private String name;
