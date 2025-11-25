ecc8e4427f2e1e62558bfbd7f3c1fa7ad192a265
NaderMohamed325 <nadoorclasher@gmail.com>
2025-11-25 17:38:15 +0200

    feat: Enhance TripServiceImpl to include TripRepository, save trip details upon initialization, and refactor code for clarity

--- src/main/java/com/yaquodorg/yaquod/service/trip/TripServiceImpl.java
+++ src/main/java/com/yaquodorg/yaquod/service/trip/TripServiceImpl.java
@@ -1,25 +1,23 @@
 package com.yaquodorg.yaquod.service.trip;
 
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-
+import com.yaquodorg.yaquod.dtos.EtaStatusDto;
+import com.yaquodorg.yaquod.dtos.InitTripDto;
 import com.yaquodorg.yaquod.entity.*;
 import com.yaquodorg.yaquod.repository.RequestRepository;
+import com.yaquodorg.yaquod.repository.TripRepository;
 import com.yaquodorg.yaquod.repository.UserRepository;
+import com.yaquodorg.yaquod.service.mqtt.MqttService;
+import com.yaquodorg.yaquod.service.vehicle.VehicleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 
-import com.yaquodorg.yaquod.dtos.EtaStatusDto;
-import com.yaquodorg.yaquod.dtos.InitTripDto;
-import com.yaquodorg.yaquod.service.mqtt.MqttService;
-import com.yaquodorg.yaquod.service.vehicle.VehicleService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,8 +26,9 @@ public class TripServiceImpl implements TripService {
 
     private final MqttService mqttService;
     private final VehicleService vehicleService;
-    private  final RequestRepository requestRepository;
+    private final RequestRepository requestRepository;
     private final UserRepository userRepository;
+    private final TripRepository tripRepository;
 
     private static final String TOPIC_INIT_TRIP = "topic/trip/init";
     private static final String TOPIC_ETA_TRIP = "topic/trip/eta";
@@ -42,6 +41,7 @@ public class TripServiceImpl implements TripService {
         Vehicle vehicle = vehicles.get(0);
         String vinNumber = vehicle.getVinNumber();
 
+        User user = requestRepository.findById(requestId).orElseThrow().getUser();
         // build dto
         InitTripDto initTripDto = InitTripDto.builder()
                 .vinNumber(vinNumber)
@@ -51,6 +51,16 @@ public class TripServiceImpl implements TripService {
                 .endLong(endLong)
                 .endLat(endLat)
                 .build();
+
+        tripRepository.save(Trip.builder()
+                .request(requestRepository.findById(requestId).orElseThrow(() -> new RuntimeException("Request not " +
+                        "found!")))
+                .vehicle(vehicle)
+                .user(user)
+                .status(TripStatus.INITIATED)
+                .startedAt(new Timestamp(new Date().getTime()))
+                .build());
+
 
         // publish to broker
         mqttService.publish(TOPIC_INIT_TRIP, initTripDto);
@@ -79,8 +89,8 @@ public class TripServiceImpl implements TripService {
                 .createdAt(new Timestamp(new Date().getTime()))
                 .build();
 
-                Request savedRequest = requestRepository.save(request);
-        initTrip(savedRequest.getId(),  startLong,  startLat,  endLong,  endLat);
+        Request savedRequest = requestRepository.save(request);
+        initTrip(savedRequest.getId(), startLong, startLat, endLong, endLat);
         return savedRequest;
     }
 
@@ -88,7 +98,7 @@ public class TripServiceImpl implements TripService {
     public RequestStatus getRequestStatusByRequestId(Long requestId) {
         Request request = requestRepository.findById(requestId)
                 .orElseThrow(() -> new RuntimeException("Request not found!"));
-        return  request.getStatus();
+        return request.getStatus();
     }
 
 
