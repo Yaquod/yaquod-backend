e35beccc461bd150ffa00ffcb463510e5a963ea4
zkrallah <muhammad.heshamyt@gmail.com>
2025-11-28 20:43:27 +0200

    refactor: Delegating user info extraction to spring security instead of manually calling the jwt and user services APIs.

--- src/main/java/com/yaquodorg/yaquod/controller/TripController.java
+++ src/main/java/com/yaquodorg/yaquod/controller/TripController.java
@@ -3,20 +3,19 @@ package com.yaquodorg.yaquod.controller;
 import static com.yaquodorg.yaquod.response.ApiResponse.createSuccessResponse;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yaquodorg.yaquod.dtos.TripRequestDto;
 import com.yaquodorg.yaquod.entity.Request;
+import com.yaquodorg.yaquod.entity.User;
 import com.yaquodorg.yaquod.response.ApiResponse;
-import com.yaquodorg.yaquod.service.jwt.JwtService;
 import com.yaquodorg.yaquod.service.request.RequestService;
-import com.yaquodorg.yaquod.service.user.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,17 +27,12 @@ import lombok.extern.slf4j.Slf4j;
 public class TripController {
 
     private final RequestService requestService;
-    private final JwtService jwtService;
-    private final UserService userService;
 
     @PostMapping("/request")
     public ResponseEntity<ApiResponse<Request>> createRequest(@RequestBody TripRequestDto tripRequestDto,
-            @RequestHeader("Authorization") String token) {
+            @AuthenticationPrincipal User user) {
         try {
-            String userEmail = jwtService.getEmailFromToken(token.substring(7));
-            Long userId = userService.getUser(userEmail).orElseThrow().getId();
-
-            Request request = requestService.createRequest(userId,
+            Request request = requestService.createRequest(user.getId(),
                     tripRequestDto.getStartLong(),
                     tripRequestDto.getStartLat(),
                     tripRequestDto.getEndLong(),
