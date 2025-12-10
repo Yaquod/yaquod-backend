b52e7cd7cd942aaa0237e9ebedfa09e0ff39faab
NaderMohamed325 <nadoorclasher@gmail.com>
2025-12-10 13:09:43 +0200

    feat: Add VIN validation to vehicle DTOs with custom annotation and validator

--- src/main/java/com/yaquodorg/yaquod/dtos/VehicleDto.java
+++ src/main/java/com/yaquodorg/yaquod/dtos/VehicleDto.java
@@ -1,10 +1,12 @@
 package com.yaquodorg.yaquod.dtos;
 
+import com.yaquodorg.yaquod.utils.ValidVIN;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class VehicleDto {
+    @ValidVIN
     private String vinNumber;
 }
