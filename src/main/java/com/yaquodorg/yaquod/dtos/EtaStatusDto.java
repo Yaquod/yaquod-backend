4c0926a754c4547d114d63084a8f19afc221f99c
NaderMohamed325 <nadoorclasher@gmail.com>
2025-12-10 13:23:46 +0200

    feat: Add validation annotations to DTOs ensuring stricter data integrity

--- src/main/java/com/yaquodorg/yaquod/dtos/EtaStatusDto.java
+++ src/main/java/com/yaquodorg/yaquod/dtos/EtaStatusDto.java
@@ -1,6 +1,8 @@
 package com.yaquodorg.yaquod.dtos;
 
 import com.yaquodorg.yaquod.entity.RequestStatus;
+import com.yaquodorg.yaquod.utils.ValidVIN;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,11 +11,15 @@ import lombok.Data;
 public class EtaStatusDto {
     private RequestStatus status;
 
+    @ValidVIN
     private String vinNumber;
 
+    @NotNull(message = "Request ID cannot be null")
     private Long requestId;
 
+   @NotNull(message = "Estimated time cannot be null")
     private double estimatedTime;
 
+  @NotNull(message = "Estimated fare cannot be null")
     private double estimatedFare;
 }
--- src/main/java/com/yaquodorg/yaquod/dtos/UpdateVehicleLocationDto.java
+++ src/main/java/com/yaquodorg/yaquod/dtos/UpdateVehicleLocationDto.java
@@ -1,10 +1,21 @@
 package com.yaquodorg.yaquod.dtos;
 
+import com.yaquodorg.yaquod.utils.ValidVIN;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateVehicleLocationDto {
+    @ValidVIN
     private String vinNumber;
+    @NotNull
+    @DecimalMin("-180.0")
+    @DecimalMax("180.0")
     private double longitude;
+    @NotNull
+    @DecimalMin("-90.0")
+    @DecimalMax("90.0")
     private double latitude;
 }
